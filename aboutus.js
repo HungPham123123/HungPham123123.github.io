91d74c49c5280e5cdc576ce4e82c8beb64d0c07d
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-11-16 00:53:56 +0700

    added slick slider on about us

--- aboutus.js
+++ aboutus.js
@@ -10,3 +10,40 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+$('.image-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1446,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1230,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, 
+    {
+    breakpoint: 800,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  ]
+});
