6b698bf1eb2b215640aa0dfb5d4e77f3e1b5b6d5
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-11-30 23:14:21 +0700

    product done, need responsive

---
+++ product.js
@@ -0,0 +1,128 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var checkboxes = document.querySelectorAll('.clickable input[type="checkbox"]');
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('click', function (event) {
+
+            event.stopPropagation();
+        });
+    });
+
+    var lis = document.querySelectorAll('.clickable');
+
+    lis.forEach(function (li) {
+        li.addEventListener('click', function () {
+            var checkbox = li.querySelector('input[type="checkbox"]');
+            checkbox.checked = !checkbox.checked;
+        });
+
+        var label = li.querySelector('label');
+        if (label) {
+            label.addEventListener('click', function () {
+                var checkbox = li.querySelector('input[type="checkbox"]');
+                checkbox.checked = !checkbox.checked;
+            });
+        }
+    });
+});
+
+function toggleIcon(button) {
+    button.classList.toggle('clicked');
+}
+
+function toggleGenderList() {
+    var genderList = document.getElementById("genderList");
+    genderList.style.display = (genderList.style.display === "none" || genderList.style.display === "") ? "block" : "none";
+}
+
+function togglePriceList() {
+    var priceList = document.getElementById("priceList");
+    priceList.style.display = (priceList.style.display === "none" || priceList.style.display === "") ? "block" : "none";
+}
+
+function toggleSaleList() {
+    var saleList = document.getElementById("saleList");
+    saleList.style.display = (saleList.style.display === "none" || saleList.style.display === "") ? "block" : "none";
+}
+
+function toggleColorList() {
+    var colorList = document.getElementById("colourList");
+    colorList.style.display = (colorList.style.display === "none" || colorList.style.display === "") ? "block" : "none";
+}
+
+function toggleBrandList() {
+    var brandList = document.getElementById("brandList");
+    brandList.style.display = (brandList.style.display === "none" || brandList.style.display === "") ? "block" : "none";
+}
+
+function toggleSportsList() {
+    var sportsList = document.getElementById("sportsList");
+    sportsList.style.display = (sportsList.style.display === "none" || sportsList.style.display === "") ? "block" : "none";
+}
+
+function toggleAthletesList() {
+    var athletesList = document.getElementById("athletesList");
+    athletesList.style.display = (athletesList.style.display === "none" || athletesList.style.display === "") ? "block" : "none";
+}
+
+const menuBar = document.querySelector('.filter-content .hide-show');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+    sidebar.classList.toggle('hide');
+})
+
+
+
+
+function toggleColumns() {
+    var sidebarContainer = document.getElementById('sidebar-container');
+    var mainContent = document.getElementById('main-content');
+
+    sidebarContainer.classList.toggle('col-md-0');
+    sidebarContainer.classList.toggle('col-md-2');
+
+    mainContent.classList.toggle('col-md-10');
+    mainContent.classList.toggle('col-md-12');
+}
+
+
+
+
+function toggleSortOption() {
+    var sortOption = document.getElementById('sort-option');
+    sortOption.classList.toggle('active');
+}
+
+function updateSortBy(value, button) {
+    var sortByText = document.getElementById('sort-by-text');
+    var buttons = document.querySelectorAll('.dropdown-option');
+
+
+    buttons.forEach(function(btn) {
+        btn.classList.remove('selected');
+        btn.classList.remove('disabled');
+        btn.removeAttribute('disabled');
+    });
+
+
+    button.classList.add('selected');
+
+
+    button.classList.add('disabled');
+    button.setAttribute('disabled', 'true');
+
+
+    buttons.forEach(function(btn) {
+        if (btn.classList.contains('selected')) {
+            btn.style.color = '#707072';
+        } else {
+            btn.style.color = ''; 
+        }
+    });
+
+
+    sortByText.innerHTML = 'Sort By: ' + value;
+
+    toggleSortOption();
+}
