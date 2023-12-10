14286a3d04ef520bff93b69f818ccf8829433406
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-12-10 19:29:12 +0700

    1

--- Shoes/JS/scroll.js
+++ Shoes/JS/scroll.js
@@ -2,9 +2,9 @@
 window.addEventListener("scroll", function () {
     var header = document.getElementById("header");
     console.log("ScrollY:", window.scrollY);
-    if (window.scrollY > 55) {
+    if (window.scrollY > 110) {
       header.classList.add("sticky");
-    } else {
+    } else if (window.scrollY === 0) {
       header.classList.remove("sticky");
     }
   });
