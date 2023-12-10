f2b561559e5193553494169b56ef8c7378fccb25
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-12-10 22:17:48 +0700

    1

--- Shoes/JS/contact.js
+++ Shoes/JS/contact.js
@@ -1,18 +1,14 @@
 document.getElementById('openLiveChatBtn').addEventListener('click', function() {
-    // Trigger the live chat script
     if (window.HubSpotConversations) {
       window.HubSpotConversations.widget.open();
     }
   });
 
   function validateForm() {
-    // Resetting previous errors
     resetErrors();
 
-    // Flag to check if there are any errors
     let hasErrors = false;
 
-    // Validate Name
     const nameInput = document.getElementById('name');
     const nameError = document.getElementById('nameError');
     if (!nameInput.value.trim()) {
@@ -21,7 +17,6 @@ document.getElementById('openLiveChatBtn').addEventListener('click', function() 
         nameError.textContent = 'Please fill out this field.';
     }
 
-    // Validate Email
     const emailInput = document.getElementById('email');
     const emailError = document.getElementById('emailError');
     if (!emailInput.value.trim() || !isValidEmail(emailInput.value.trim())) {
@@ -30,7 +25,6 @@ document.getElementById('openLiveChatBtn').addEventListener('click', function() 
         emailError.textContent = 'Please fill out this field with a valid email address.';
     }
 
-    // Validate Message
     const messageInput = document.getElementById('message');
     const messageError = document.getElementById('messageError');
     if (!messageInput.value.trim()) {
@@ -39,22 +33,19 @@ document.getElementById('openLiveChatBtn').addEventListener('click', function() 
         messageError.textContent = 'Please fill out this field.';
     }
 
-    // Display general error message if there are errors
     const generalError = document.getElementById('generalError');
     if (hasErrors) {
         generalError.textContent = 'One or more fields have an error. Please check and try again.';
         return false;
     }
 
-    return true; // Form submission will proceed if no errors
+    return true;
 }
 
-// Helper function to mark input as error
 function markAsError(inputElement) {
     inputElement.classList.add('error');
 }
 
-// Helper function to reset errors
 function resetErrors() {
     const errorInputs = document.querySelectorAll('.error');
     errorInputs.forEach(input => {
@@ -67,10 +58,7 @@ function resetErrors() {
     });
 }
 
-// Helper function to check if the email is valid
 function isValidEmail(email) {
-    // You can use a more sophisticated email validation logic here
-    // This is a basic example
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
