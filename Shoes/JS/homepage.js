41e114a3f6c38938b4ab21ddb61769500ecdf10b
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-12-10 21:44:47 +0700

    add backtotop

--- Shoes/JS/homepage.js
+++ Shoes/JS/homepage.js
@@ -77,3 +77,17 @@ qtyPlus.addEventListener('click', function () {
 });
 });
 
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(this).scrollTop() > 900){
+          $('#backtop').fadeIn();
+      } else {
+          $('#backtop').fadeOut();
+      }
+  });
+  $("#backtop").click(function(){
+      $('html, body').animate({
+          scrollTop: 0
+      }, 300); 
+  });
+});
