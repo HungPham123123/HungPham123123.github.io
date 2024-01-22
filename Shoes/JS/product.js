a9169762a6655469863d3ea7f98f132ffbe735a9
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2024-01-22 15:27:02 +0700

    test file json

--- Shoes/JS/product.js
+++ Shoes/JS/product.js
@@ -161,3 +161,76 @@ function closeFilters () {
     applyFilter.classList.remove('applyfiltermobile')
     closeFilter.classList.remove('filterclose-button')
 }
+
+
+
+
+
+
+
+
+const productRowContent = document.querySelector(".product-row");
+const saleCheckbox = document.getElementById("saleCheckbox1");
+
+const jsonProduct = "../product.json";
+
+let productData = [];
+
+const renderProducts = () => {
+    const filteredProducts = productData.filter(product => {
+        return saleCheckbox.checked ? product.discount > 0 : true;
+    });
+
+    productRowContent.innerHTML = "";
+    filteredProducts.forEach(product => {
+        const { id, name, price, img, discount } = product;
+
+        const discountedPrice = (price * (100 - discount) / 100).toFixed(2);
+
+        productRowContent.innerHTML += `
+            <div class="product-box" data-product-id="${id}">
+                <div class="product">
+                    <div class="img-product">
+                        <a href="">
+                            <img style="width: 100%;" src="${img}" alt="${name}">
+                        </a>
+
+                        <ul class="product-icon">
+                            <li class="add-cart mr-0">
+                                <a href="">
+                                    <i class="fa-solid fa-bag-shopping icon-1"></i>
+                                </a>
+                            </li>
+                            <li class="view-product mr-0">
+                                <a href="">
+                                    <i class="fa-solid fa-magnifying-glass icon-2"></i>
+                                </a>
+                            </li>
+                            <li class="add-favorite mr-0">
+                                <a href="">
+                                    <i class="fa-regular fa-heart icon-3"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <h4 class="product-title">
+                        <a href="">${name}</a>
+                    </h4>
+                    <p class="product-price">
+                        ${discount > 0 ? `<s class="">$${price}</s>` : ''}
+                        <span class="">$${discountedPrice}</span>
+                    </p>
+                </div>  
+            </div>
+        `;
+    });
+};
+
+fetch(jsonProduct)
+    .then(response => response.json())
+    .then(data => {
+        productData = data;
+        renderProducts();
+    });
+
+saleCheckbox.addEventListener("change", renderProducts);
